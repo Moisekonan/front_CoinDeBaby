083f277de7dce2af757318999e7b1d9392faa9b1
Moisekonan <kouassimoise.konan@epinesdorsales.com>
2024-05-01 21:34:18 +0000

    first commit

---
+++ src/app/shared/listarticle/listarticle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listarticle',
+  standalone: true,
+  imports: [],
+  templateUrl: './listarticle.component.html',
+  styleUrl: './listarticle.component.css'
+})
+export class ListarticleComponent {
+
+}
